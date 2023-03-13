5405b54effeae6c62a8780ad7b8e3fd242336f98
Natalia <nlaputina@gmail.com>
2023-03-13 12:44:58 +0200

    3 functions in 1

--- statement/urls.py
+++ statement/urls.py
@@ -4,6 +4,4 @@ from .views import *
 
 urlpatterns = [
     path('', show_list_expenses),
-    path('period_list/', show_list_for_period),
-    path('list_on_categories/', list_on_categories)
 ]
--- statement/views.py
+++ statement/views.py
@@ -1,36 +1,46 @@
-from django.http import HttpResponse
 from django.http import JsonResponse
 from django.db.models import Q
 from django.shortcuts import render
 
 from django.views.decorators.csrf import csrf_exempt
-from .models import *
+from .models import Expenses
 
 
 @csrf_exempt
 def show_list_expenses(request):
     qs = Expenses.objects.all()
-    return JsonResponse([note.to_dict() for note in qs], safe=False)
+    qs_period = show_list_for_period(request, qs)
+    qs_category = list_on_categories(request, qs_period)
+    return JsonResponse([note.to_dict() for note in list(qs_category)], safe=False)
 
 
-@csrf_exempt
-def show_list_for_period(request):
+def show_list_for_period(request, qs):
     start_point = request.GET.get('start_point')
     final_point = request.GET.get('final_point')
-    list_for_period = Expenses.objects.filter(date__range=(start_point, final_point))
-    return JsonResponse([note.to_dict() for note in list_for_period], safe=False)
+    if start_point and final_point:
+        qs_period = qs.filter(date__range=(start_point, final_point))
+    elif start_point and not final_point:
+        qs_period = qs.filter(date__gte=start_point)
+    elif final_point and not start_point:
+        qs_period = qs.filter(date__lte=final_point)
+    else:
+        qs_period = qs
+    return qs_period
 
 
-def list_on_categories(request):
+def list_on_categories(request, qs_period):
     if request.GET.get('category') == 'food':
-        qs = Expenses.objects.filter(Q(description__contains='FROUTOPIA') | Q(description__contains='ALPHAMEGA') | Q(description__contains='sweets')|Q(description__contains='MELIS'))
+        qs_category = qs_period.filter(Q(description__contains='FROUTOPIA') | Q(description__contains='ALPHAMEGA') | Q(description__contains='sweets')|Q(description__contains='MELIS'))
     elif request.GET.get('category') == 'outside':
-        qs = Expenses.objects.filter(Q(description__contains='ZORBAS') | Q(description__contains='NOMAD') | Q(description__contains='WOLT')|Q(description__contains='RESTAURANT') | Q(description__contains='BAR') | Q(description__contains='COFFEE'))
+        qs_category = qs_period.filter(Q(description__contains='ZORBAS') | Q(description__contains='NOMAD') | Q(description__contains='WOLT')|Q(description__contains='RESTAURANT') | Q(description__contains='BAR') | Q(description__contains='COFFEE'))
     elif request.GET.get('category') == 'car':
-        qs = Expenses.objects.filter(Q(description__contains='ESSO') | Q(description__contains='PETROLINA'))
+        qs_category = qs_period.filter(Q(description__contains='ESSO') | Q(description__contains='PETROLINA'))
     elif request.GET.get('category') == 'health':
-        qs = Expenses.objects.filter(Q(description__contains='PHARMA') | Q(description__contains='MEDICAL'))
+        qs_category = qs_period.filter(Q(description__contains='PHARMA') | Q(description__contains='MEDICAL'))
     elif request.GET.get('category') == 'children':
-        qs = Expenses.objects.filter(Q(description__contains='MAVROS') | Q(description__contains='WONDERLAND') | Q(description__contains='JUMBO'))
-    list_category = list(qs)
-    return JsonResponse([note.to_dict() for note in list_category], safe=False)
+        qs_category = qs_period.filter(Q(description__contains='MAVROS') | Q(description__contains='WONDERLAND') | Q(description__contains='JUMBO'))
+    else:
+        qs_category = qs_period
+
+    return qs_category
+
